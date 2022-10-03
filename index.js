8b43e61509751128333812d91c31b52c4ee6e4e8
Kyle Brackman <kyle.a.brackman@gmail.com>
2022-10-02 18:36:27 -0600

    commit tree 6531413ca2303a84a68f3c5b6505f6fd3ce96df4

--- index.js
+++ index.js
@@ -1,1 +1,87 @@
-// Write your solution here!
+// practice session below
+
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat() {
+    return cats.push("Ralph")
+}
+
+function destructivelyPrependCat() {
+    return cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift()
+}
+
+function appendCat() {
+    return ([...cats, "Broom"])
+}
+
+function prependCat() {
+    return ["Arnold", ...cats]
+}
+
+function removeLastCat() {
+    return cats.slice(0,2)
+}
+
+function removeFirstCat() {
+    return cats.slice(1)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const cats = ["Milo", "Otis", "Garfield"]
+
+
+// function destructivelyAppendCat () {
+//     return cats.push("Ralph");
+// }
+
+// function destructivelyPrependCat () {
+//     return cats.unshift("Bob");
+// }
+
+// function destructivelyRemoveLastCat() {
+//     return cats.pop();
+// }
+
+// function destructivelyRemoveFirstCat() {
+//     return cats.shift();
+// }
+
+// function appendCat() {
+//     return [...cats, "Broom"];
+// }
+
+
+// function prependCat() {
+//     return ["Arnold", ...cats];
+// }
+
+// function removeLastCat() {
+//     return cats.slice(0,2)
+// }
+
+// function removeFirstCat() {
+//     return cats.slice(-2)
+// }
